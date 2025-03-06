c4444f4939c22e35e83a7358f85957362d3be8e3
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2025-03-06 15:24:27 -0300

    Public e private

--- ex017/main.js
+++ ex017/main.js
@@ -8,5 +8,26 @@ class Estudante {
         return `Aluno: ${this.nomeEstudante}\nMatrícula:  ${this.codigoEstudante}`;
     }
 }
+class ContaBacaria {
+    constructor(nome) {
+        this.nome = nome;
+        this.saldo = 0;
+    }
+    definirSenha(novaSenha) {
+        if (this.senha === null) {
+            this.senha = novaSenha;
+            return;
+        }
+        console.log("Essa conta já possui senha.");
+    }
+    alterarSenha(senhaAntiga, novaSenha) {
+        if (this.senha === senhaAntiga) {
+            this.senha = novaSenha;
+            return;
+        }
+        console.log("Não foi possível alterar a senha.");
+    }
+}
 const estudanteJoao = new Estudante(2224560, "João Marcos");
 console.log(`${estudanteJoao}`);
+const contaNova = new ContaBacaria("Mario");
--- ex017/main.ts
+++ ex017/main.ts
@@ -12,5 +12,34 @@ class Estudante {
     }
 }
 
-const estudanteJoao = new Estudante(2224560, "João Marcos")
-console.log(`${estudanteJoao}`)
+class ContaBacaria {
+    public nome: string; 
+    private saldo: number;
+    private senha?: number;
+    
+    constructor(nome: string) {
+        this.nome = nome
+        this.saldo = 0
+    }
+
+    definirSenha(novaSenha: number){
+        if (this.senha === null){
+            this.senha = novaSenha
+            return
+        }
+        console.log("Essa conta já possui senha.")
+    }
+
+    alterarSenha(senhaAntiga: number, novaSenha: number) {
+        if (this.senha === senhaAntiga){
+            this.senha = novaSenha
+            return
+        }
+        console.log("Não foi possível alterar a senha.")
+    }
+}
+
+/*const estudanteJoao = new Estudante(2224560, "João Marcos")
+console.log(`${estudanteJoao}`)*/
+
+const contaNova = new ContaBacaria("Mario")
